bc79ce4ce2f5d4664821618a326fbe9dff9c7a73
Rafael Alonso <rafael.palonso@gmail.com>
2021-08-02 10:02:33 -0300

    now i saved the file :D

--- ds-project-650/hello.py
+++ ds-project-650/hello.py
@@ -1,11 +1,7 @@
-<<<<<<< HEAD
-
-
-def hello():
-  print('Hi Im max!')
-=======
 from termcolor import colored
 
 def hello():
   print(colored('hello world', 'blue'))
->>>>>>> master
+
+def say_hi():
+  print('Hi Im max!')
